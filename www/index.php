22bf46f71a34fc36a377154d15a65ea57aa31257
Alison Butcher <alison@alisonbutcher.com>
2022-04-18 17:00:36 +1000

    refactored mssql

--- www/index.php
+++ www/index.php
@@ -1,1 +1,28 @@
-<?php phpinfo(); ?>
+<?php 
+        $servername = "tcp:myserver.database.windows.net,1433";
+        $connectionOptions = array("Database"=>"Adventureworks",
+            "Uid"=>"sa", "PWD"=>"ygedr2d2;1234");
+        $conn = sqlsrv_connect($servername, $connectionOptions);
+        if($conn == false) {
+            die(FormatErrors(sqlsrv_errors()));
+        } else {
+            echo "success";
+        }
+
+
+echo "hello2" 
+
+
+// function OpenConnection() {
+//     $servername = "tcp:myserver.database.windows.net,1433";
+//     $connectionOptions = array("Database"=>"Adventureworks",
+//         "Uid"=>"sa", "PWD"=>"ygedr2d2;1234");
+//     $conn = sqlsrv_connect($servername, $connectionOptions);
+//     if($conn == false) {
+//         die(FormatErrors(sqlsrv_errors()));
+//     }
+
+//     return $conn;
+// }
+
+?>
